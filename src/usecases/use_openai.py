cfe0485c6dd8c68e51d34ce6b3c2a3a079333cd1
Manu Espinosa <jose_el@tesch.edu.mx>
2023-10-28 23:13:24 -0600

    UPDATE: change chatgpt promt

--- src/usecases/use_openai.py
+++ src/usecases/use_openai.py
@@ -1,7 +1,7 @@
 from frameworks.openai import compare_products
 
 promts={
-    'compare': "Compara objetivamente estos productos y entregame la mejor opcion segun su precio y caracteristicas en un parrafo"
+    'compare': "Compara objetivamente estos productos y entregame la mejor opcion segun su precio y caracteristicas en un parrafo e indica la tienda en donde se encuentra"
     }
 
 def use_openai(products):
