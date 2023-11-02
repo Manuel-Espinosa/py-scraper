1c67c193797ceb74868449a5a94629ade565424e
Manu Espinosa <jose_el@tesch.edu.mx>
2023-11-01 23:11:13 -0600

    FIX: CORS policy added

--- src/app.py
+++ src/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from interface_adapters.blueprint import main_blueprint
+from flask_cors import CORS  # Import CORS
 import os
 
 SCRAPER_PORT = int(os.environ.get("SCRAPER_PORT", 5000))
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(main_blueprint)
 
 if __name__ == '__main__':
